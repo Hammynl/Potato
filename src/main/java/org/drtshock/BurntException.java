8d1ebc0c37eb1e59080ae115628f61f5b716ef2c
Lars <squidcrafteu@gmail.com>
2019-04-22 01:14:33 +0200

    Update BurntException.java

--- src/main/java/org/drtshock/BurntException.java
+++ src/main/java/org/drtshock/BurntException.java
@@ -6,11 +6,10 @@ package org.drtshock;
 public class BurntException extends Exception {
 
     public BurntException(int degrees) {
-        super("Potato is badly burnt by trying to boil it at " + degrees + " degrees!!");
+        super("Potato is overcooked by trying to boil it at " + degrees + " degrees!!");
     }
 
     public BurntException(long bakeTime) {
         super("Potato is badly burnt by baking for too long!! (" + bakeTime + "ms)");
     }
-
 }
